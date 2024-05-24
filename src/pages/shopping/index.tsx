c785ed93d5f1cddf9edc4b0dfc6bcfad1c471c0b
lorenzoappo12 <Lorenzo.appolinaire@outlook.com>
2024-05-24 05:34:30 -0500

    WIP: creating shopping component

---
+++ src/pages/shopping/index.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material"
+import './index.scss';
+
+export const Shopping = () => {
+    return (
+        <div className="product-container">
+            <Box className='product-header'>
+                <Typography>Shopping List</Typography>
+            </Box>
+        </div>
+    )
+}
